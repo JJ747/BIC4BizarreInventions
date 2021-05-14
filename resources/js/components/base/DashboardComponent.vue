ac04daf92e5c260471b85e76c5afafb1d372e383
Julia Janusch <ic19b074@technikum-wien.at>
2021-05-14 16:00:50 +0200

    Dashboard Component Added

---
+++ resources/js/components/base/DashboardComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <div class="columns is-multiline">
+            <div class="card blog-card column is-half is-offset-one-quarter">
+                <header class="card-header">
+                    <h1 class="card-header-title" v-text="title" />
+                </header>
+                <div class="card-content">
+                    <div class="content">
+                        <slot></slot>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            title: {
+                required: true,
+                type: String
+            }
+        }
+    }
+</script>
